cc146f0b7fc8ea7b9eb6156422ce069cdad31b3f
HowEmbarrassing <starkwaizer@gmail.com>
2020-07-05 20:55:40 +0400

    Added validation for a contact form

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PKWebApp.Models;
+using PKWebApp.Services;
+
+namespace PKWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMailService _mailService;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }        
+        
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }        
+        
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+                _mailService.SendMessage("my email", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                // Show the error
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
