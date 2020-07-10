009af615bc21047da14dd54ed6a9de14159320ec
HowEmbarrassing <starkwaizer@gmail.com>
2020-07-10 23:41:36 +0400

    Added Data/Entities with Classes for SQL Server

---
+++ Data/Entities/Speciality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKWebApp.Data.Entities
+{
+    public class Speciality
+    {
+        public int SpecialityId { get; set; }
+        public string SpecialityTitle { get; set; }
+    }
+}
---
+++ Data/PKContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PKWebApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKWebApp.Data
+{
+    public class PKContext : DbContext
+    {
+        public DbSet<Contractor> Contractors { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
